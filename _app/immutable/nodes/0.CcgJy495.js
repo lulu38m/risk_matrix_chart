9d8fcd6534cd6ec542192e1547372e7077d9d458
lucas <lucas.moreau@supinfo.com>
2025-06-22 18:46:50 +0200

    Updates

---
+++ _app/immutable/nodes/0.CcgJy495.js
@@ -0,0 +1,1 @@
+import{c as n,a}from"../chunks/nsCmRsXm.js";import{s}from"../chunks/CzluxtiQ.js";import{s as p}from"../chunks/B5ymDBPr.js";const i=!0,f=Object.freeze(Object.defineProperty({__proto__:null,prerender:i},Symbol.toStringTag,{value:"Module"}));function u(o,r){var e=n(),t=s(e);p(t,()=>r.children),a(o,e)}export{u as component,f as universal};
