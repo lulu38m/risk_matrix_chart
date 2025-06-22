88fb40314b39a99480e3e7f5e069194986189d78
lucas <lucas.moreau@supinfo.com>
2025-06-22 21:36:48 +0200

    Update base path in svelte.config.js for production environment

--- svelte.config.js
+++ svelte.config.js
@@ -8,7 +8,7 @@ const config = {
 	kit: {
 		appDir: 'app',
 		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/risk_matrix_chart\n' : '',
+			base: process.env.NODE_ENV === 'production' ? '/risk_matrix_chart' : '',
 		},
 		adapter: adapter({
 			pages: 'build',
